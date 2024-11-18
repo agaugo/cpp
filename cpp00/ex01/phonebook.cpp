8be758cd8dc7e1972f579c1cd6515eaa7c8bd066
Hugo Flohil De Sousa <hflohil-@f1r3s6.codam.nl>
2024-11-18 17:19:23 +0100

    Almost done

--- cpp00/ex01/phonebook.cpp
+++ cpp00/ex01/phonebook.cpp
@@ -1,7 +1,88 @@
-#include "iostream"
+#include <vector>
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <sstream>
+#include "phonebook.hpp"
+
+int addContact(Phonebook& phonebook)
+{
+    std::string firstName, lastName, nickname, phoneNumber, darkestSecret;
+
+    do {
+        std::cout << "Enter first name: ";
+        std::getline(std::cin, firstName);
+        if (firstName.empty()) {
+            std::cout << "First name cannot be empty. Please try again.\n";
+        }
+    } while (firstName.empty());
+
+    do {
+        std::cout << "Enter last name: ";
+        std::getline(std::cin, lastName);
+        if (lastName.empty()) {
+            std::cout << "Last name cannot be empty. Please try again.\n";
+        }
+    } while (lastName.empty());
+
+    do {
+        std::cout << "Enter nickname: ";
+        std::getline(std::cin, nickname);
+        if (nickname.empty()) {
+            std::cout << "Nickname cannot be empty. Please try again.\n";
+        }
+    } while (nickname.empty());
+
+    do {
+        std::cout << "Enter phone number: ";
+        std::getline(std::cin, phoneNumber);
+        if (phoneNumber.empty()) {
+            std::cout << "Phone number cannot be empty. Please try again.\n";
+        }
+    } while (phoneNumber.empty());
+
+    do {
+        std::cout << "Enter darkest secret: ";
+        std::getline(std::cin, darkestSecret);
+        if (darkestSecret.empty()) {
+            std::cout << "Darkest secret cannot be empty. Please try again.\n";
+        }
+    } while (darkestSecret.empty());
+
+    Contact newContact(firstName, lastName, nickname, phoneNumber, darkestSecret);
+    phonebook.addContact(newContact);
+
+    return (1);
+}
+
+int searchPhonebook(Phonebook& phonebook)
+{
+    std::string input;
+    std::size_t index;
+
+    phonebook.displayContacts();
+    std::cout << "Enter index for user to be displayed: ";
+    std::getline(std::cin, input);
+
+    std::stringstream(input) >> index;
+    if (index < 1 || index > phonebook.contacts.size()) {
+        std::cout << "Invalid index. Enter a number between 1 and " << phonebook.contacts.size() << "\n";
+        return (-1);  
+    }
+    const Contact& contact = phonebook.contacts[index - 1];
+    std::cout 
+        << std::right << "First Name: " << contact.firstName << "\n"
+        << std::right << "Last Name: " << contact.lastName << "\n"
+        << std::right << "Nickname: " << contact.nickname << "\n"
+        << std::right << "Phone Number: " << contact.phoneNumber << "\n"
+        << std::right << "Darkest Secret:" << contact.darkestSecret << "\n";
+    
+    return (0);
+}
 
 int	main(void)
 {
+    Phonebook phonebook;
 
     std::string input;
     std::cout << "Welcome to the Phonebook!" << std::endl;
@@ -12,9 +93,9 @@ int	main(void)
         if (input == "EXIT")
             break;
         else if (input == "ADD")
-            std::cout << "Adding a new contact..." << std::endl;
+            addContact(phonebook);
         else if (input == "SEARCH")
-            std::cout << "Searching contacts..." << std::endl;
+            searchPhonebook(phonebook);
         else
             std::cout << "Invalid command!" << std::endl;
             
@@ -24,4 +105,3 @@ int	main(void)
     std::cout << "Goodbye!" << std::endl;
     return 0;
 }
-
---
+++ cpp00/ex01/phonebook.hpp
@@ -0,0 +1,52 @@
+#include "iostream"
+#include "vector"
+#include <iostream>
+#include <iomanip>
+#include <string>
+
+class Contact
+{
+    public:
+
+    std::string firstName;
+    std::string lastName;
+    std::string nickname;
+    std::string phoneNumber;
+    std::string darkestSecret;
+
+    Contact(std::string fn, std::string ln, std::string nn, std::string pn, std::string ds)
+    : firstName(fn), lastName(ln), nickname(nn), phoneNumber(pn), darkestSecret(ds) {}
+};
+
+
+class Phonebook
+{
+    public:
+    
+    std::vector <Contact> contacts;
+
+    void addContact(const Contact& newContact) {
+        contacts.push_back(newContact);
+    }
+
+    void displayContacts()
+    {
+        if (contacts.empty()) {
+            std::cout << "Phonebook is empty. \n";
+            return ;
+        }
+
+        std::cout << "PHONEBOOK:\n";
+        std::cout << "     index|first name| last name|  nickname\n";
+
+        for (size_t i = 0; i < contacts.size(); ++i)
+        {
+            const Contact& contact = contacts[i];
+            std::cout
+                << std::setw(10) << std::right << i + 1 << "|"
+                << std::setw(10) << std::right << contact.firstName.substr(0,9) << "|"
+                << std::setw(10) << std::right << contact.lastName.substr(0,9) << "|"
+                << std::setw(10) << std::right << contact.nickname.substr(0,9) << "\n";
+        }
+    }
+};
