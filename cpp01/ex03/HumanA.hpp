c8d8be4e106515c9f0cc0861e5237cd39204e808
Hugo <hugoflohildesousa@gmail.com>
2024-11-22 23:51:00 +0100

    classed added

--- cpp01/ex03/HumanA.hpp
+++ cpp01/ex03/HumanA.hpp
@@ -1,4 +1,16 @@
 #ifndef HUMANA_HPP
 #define HUMANA_HPP
 
+#include <string>
+#include "Weapon.hpp"
+
+class HumanA {
+    private:
+        std::string name;
+        Weapon& weapon;
+    public:
+        HumanA(std::string name, Weapon &weapon);
+        void setWeapon(Weapon &weapon);
+        void attack();
+}
 #endif
--- cpp01/ex03/HumanB.hpp
+++ cpp01/ex03/HumanB.hpp
@@ -1,4 +1,17 @@
 #ifndef HUMANB_HPP
 #define HUMANB_HPP
 
+#include <string>
+#include "Weapon.hpp"
+
+class HumanA {
+    private:
+        std::string name;
+        Weapon* weapon;                     //Pointer to Weapon class (not reference)
+    public:
+        HumanB(std::string name);
+        void setWeapon(Weapon &weapon);
+        void attack();
+    }
+
 #endif
